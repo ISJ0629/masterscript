0b057559344cdbcc61582c5e911ef1315ece73da
GMDMachine <64815589+ISJ0629@users.noreply.github.com>
2023-02-22 10:43:15 +0900

    Update LoadString_1.0.lua

--- LoadString_1.0.lua
+++ LoadString_1.0.lua
@@ -18,4 +18,13 @@ end
 if game.PlaceId == 6236529295 then -- train fighting sim
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end
+if game.PlaceId == 537413528 then -- BABFT
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))() -- infinity yierd
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))() --vape
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Build%20A%20Boat%20For%20Treasure/Script.lua"))() --vynixius
+    local a,b,c = "juywvm","main","babft";loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/-Roblox-Projects-/%s/%s"):format(a, b, c)))() --autofarm
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/Loader/main/Loader.lua"))() --key:Neko
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XRoLLu/Rolly_Hub/main/open-source-trash-loader.exe.yeah"))() -- autofarm,mods
+    
+end
 --loadstring(game:HttpGet("https://pastebin.com/yVdXMaFy"))() --재접 스크립트
