733e7b39d9b3868904c32bdd86b562342c263fde
GMDMachine <64815589+ISJ0629@users.noreply.github.com>
2023-03-05 21:29:32 +0900

    Update LoadString_1.0.lua

--- LoadString_1.0.lua
+++ LoadString_1.0.lua
@@ -2,7 +2,7 @@ if game.PlaceId == 6839171747 then --doors ingame
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Doors/Loader.lua"))()
     --loadstring(game:HttpGet("https://raw.githubusercontent.com/OminousVibes-Exploit/Scripts/main/doors/loadstring.lua"))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ISJ0629/scripts/master/scripts/DOORS/doors.txt"))()
-    --loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+    --loadstrlng(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end
 if game.PlaceId == 6516141723 then -- doors lobby
@@ -25,12 +25,20 @@ if game.PlaceId == 537413528 then -- BABFT
     loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))() --vape
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Build%20A%20Boat%20For%20Treasure/Script.lua"))() --vynixius
     --local a,b,c = "juywvm","main","babft";loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/-Roblox-Projects-/%s/%s"):format(a, b, c)))() --autofarm
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/Loader/main/Loader.lua"))() --key:Neko
+    --loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/Loader/main/Loader.lua"))() --key:Neko
     --loadstring(game:HttpGet("https://raw.githubusercontent.com/XRoLLu/Rolly_Hub/main/open-source-trash-loader.exe.yeah"))() -- autofarm,mods
 end
 if game.PlaceId == 3527629287 then -- big paintball
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/radjahfromdiscord/iNEXT/main/source"))()
+    --loadstring(game:HttpGet("https://raw.githubusercontent.com/radjahfromdiscord/iNEXT/main/source"))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
     loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+end
+if game.PlaceId == 5720801512 then -- 한머
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/K-hara0321/myscripts/main/KoreaMurderLocalXONTOP"))()
+end
+if game.PlaceId == 662417684 then --럭키블럭
+	loadstring(game:HttpGet("https://github.com/bruhhwtf/LUCKY-BLOCKS-Battlegrounds-GUI/raw/main/Main"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end
 --loadstring(game:HttpGet("https://pastebin.com/yVdXMaFy"))() --재접 스크립트
