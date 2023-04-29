fa187004a0f3f4bb86245b11a87e6f2f43426926
GMDMachine <64815589+ISJ0629@users.noreply.github.com>
2023-04-29 18:28:43 +0900

    iy deleted due to iymod.lua

--- LoadString_1.0.lua
+++ LoadString_1.0.lua
@@ -13,7 +13,7 @@ if game.PlaceId == 11040063484 then -- sword fighting sim
 end
 if game.PlaceId == 281489669 then --wizard tycoon
     loadstring(game:HttpGet("https://pastebin.com/raw/euFigAv1"))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+    --loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
     --BROUGHT TO YOU BY RSCRIPTS.NET--
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Wizard%20Tycoon%20-%202%20Player/2pWizardTycoonGUI"))()
 end
@@ -21,16 +21,15 @@ if game.PlaceId == 6236529295 then -- train fighting sim
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end
 if game.PlaceId == 537413528 then -- BABFT
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))() -- infinity yierd
+    --loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))() -- infinity yierd
     loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))() --vape
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Build%20A%20Boat%20For%20Treasure/Script.lua"))() --vynixius
-    --local a,b,c = "juywvm","main","babft";loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/-Roblox-Projects-/%s/%s"):format(a, b, c)))() --autofarm
-    --loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/Loader/main/Loader.lua"))() --key:Neko
+    local a,b,c = "juywvm","main","babft";loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/-Roblox-Projects-/%s/%s"):format(a, b, c)))() --autofarm
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/Loader/main/Loader.lua"))() --key:Neko
     --loadstring(game:HttpGet("https://raw.githubusercontent.com/XRoLLu/Rolly_Hub/main/open-source-trash-loader.exe.yeah"))() -- autofarm,mods
 end
 if game.PlaceId == 3527629287 then -- big paintball
-    --loadstring(game:HttpGet("https://raw.githubusercontent.com/radjahfromdiscord/iNEXT/main/source"))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/radjahfromdiscord/iNEXT/main/source"))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
     loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
 end
@@ -39,6 +38,5 @@ if game.PlaceId == 5720801512 then -- 한머
 end
 if game.PlaceId == 662417684 then --럭키블럭
 	loadstring(game:HttpGet("https://github.com/bruhhwtf/LUCKY-BLOCKS-Battlegrounds-GUI/raw/main/Main"))()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end
 --loadstring(game:HttpGet("https://pastebin.com/yVdXMaFy"))() --재접 스크립트
